f4ba4188b1b2a06b0540c177ef75ae4281250529
asraful-07 <asraful0508@gmail.com>
2024-10-23 14:46:38 +0600

    added header section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ function App() {
  
 
   const [recipeQueue, setRecipeQueue] = useState([])
+  const [preparedRecipe, setPreparedRecipe] = useState([])
+  
+
+  const handelRemove = id => {
+    // find which recipe to remove
+    const deleteRecipe = recipeQueue.find( recipe => recipe.recipe_id === id)
+
+    // remove from want to cook remove
+    const updated = recipeQueue.filter(recipe => recipe.recipe_id !== id)
+    setRecipeQueue(updated)
+    setPreparedRecipe([...preparedRecipe, deleteRecipe])
+  }
 
   const handelRecipe = recipe => {
     // console.log('uami testi ummmm', recipe)
@@ -22,12 +34,18 @@ function App() {
     
   }
 
+
+
   return (
     <>
     <Header></Header>
+    <div className='text-center w-3/4 mx-auto'>
+     <h2 className='text-4xl font-bold'> Our Recipe</h2>
+     <p className='my-2 opacity-60'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aut temporibus, illo culpa, cupiditate, consequatur explicabo nostrum blanditiis repellat necessitatibus atque debitis quisquam adipisci. Eaque tenetur dolores nam harum a. Ipsum mollitia quisquam magni cum beatae! Adipisci culpa expedita quaerat.</p>
+    </div>
     <main className='container mx-auto p-6 flex flex-col md:flex-row gap-6'>
     <Recipes handelRecipe={handelRecipe}></Recipes>
-    <Sidebar recipeQueue={recipeQueue}></Sidebar>
+    <Sidebar recipeQueue={recipeQueue} handelRemove={handelRemove} preparedRecipe={preparedRecipe}></Sidebar>
       </main>  
     </>
   )
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 // import Read from "../Read/Read";
 
-const Sidebar = ({recipeQueue}) => {
+const Sidebar = ({recipeQueue, handelRemove, preparedRecipe}) => {
     return (
         <div className="md:w-1/3 border-2 rounded">
             <div className="flex justify-center items-center my-4">
@@ -28,7 +28,7 @@ const Sidebar = ({recipeQueue}) => {
         <td>{recipe.preparing_time}</td>
         <td>{recipe.calories}</td>
         <td>
-        <button onClick={ () => console.log(recipe)} className="btn bg-green-800 text-white hover:bg-green-700 transition-all duration-300 ease-in-out"> 
+        <button onClick={ () => handelRemove(recipe.recipe_id)} className="btn bg-green-800 text-white hover:bg-green-700 transition-all duration-300 ease-in-out"> 
                            Cook
                         </button>
         </td>
@@ -37,7 +37,38 @@ const Sidebar = ({recipeQueue}) => {
     }
     </tbody>
   </table>
-</div>
+   </div>
+
+   {/* current recipe */}
+   <div className="flex justify-center items-center my-4">
+            <h1 className="text-3xl font-bold">Currently Cook:{preparedRecipe.length}</h1>
+            </div>
+            <div className="divider"></div>
+   <div className="overflow-x-auto">
+      <table className="table">
+      {/* head */}
+       <thead>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Time</th>
+          <th>Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+    {
+         preparedRecipe.map((recipe, idx) => (
+        <tr className="hover" key={idx}>
+        <th>{idx + 1}</th>
+        <td>{recipe.recipe_name}</td>
+        <td>{recipe.preparing_time}</td>
+        <td>{recipe.calories}</td>
+      </tr>
+         ))
+    }
+    </tbody>
+  </table>
+   </div>
      </div>
     );
 };
