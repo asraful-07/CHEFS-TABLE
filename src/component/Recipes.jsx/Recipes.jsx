306ed392f9f795543bfb7cb6b969dab426383a94
asraful-07 <asraful0508@gmail.com>
2024-10-26 14:12:53 +0600

    somthing chenging

--- src/component/Recipes.jsx/Recipes.jsx
+++ src/component/Recipes.jsx/Recipes.jsx
@@ -2,25 +2,28 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Recipe from "../../Recipe/Recipe";
 
-const Recipes = ({handelRecipe}) => {
-    const [recipes, setRecipes] = useState([])
+const Recipes = ({ handelRecipe }) => {
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect( () => {
-        fetch('recipes.json')
-        .then(res => res.json())
-        .then(data => setRecipes(data))
-    },[])
+  useEffect(() => {
+    fetch("recipes.json")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
+  }, []);
 
-    return (
-        <div className="md:flex md:w-2/3">
-           {/* <h1>Recipes: {recipes.length}</h1>  */}
-          <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
-          {
-            recipes.map( (recipe, idx) => <Recipe key={idx} recipe={recipe} handelRecipe={handelRecipe}></Recipe>)
-           }
-          </div>
-        </div>
-    );
+  return (
+    <div className="md:flex md:w-2/3">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
+        {recipes.map((recipe, idx) => (
+          <Recipe
+            key={idx}
+            recipe={recipe}
+            handelRecipe={handelRecipe}
+          ></Recipe>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Recipes;
